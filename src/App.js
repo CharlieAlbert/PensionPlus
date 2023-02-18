03f85bcbb464c11b49aeeb7df4e0ada6da1cc19d
CharlieAlbert <charlieraph36@gmail.com>
2023-02-18 13:07:31 +0300

    pension plus components commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
